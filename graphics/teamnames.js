6f770ff985b0922a3d7ac35ae7f4127a5e9ae1f6
themicrocheeze <themicrocheez@gmail.com>
2022-02-20 11:54:21 -0500

    Scores Work and change color with the team name

--- graphics/teamnames.js
+++ graphics/teamnames.js
@@ -4,6 +4,9 @@ const team2nameRep = nodecg.Replicant('team2name');
 const team1nameEl = document.getElementById('overwatch_team_name');
 const team2nameEl = document.getElementById('talon_team_name');
 
+const team1scoreboxEl = document.getElementById('overwatchScore');
+const team2scoreboxEl = document.getElementById('talonScore');
+
 const team1styleRep = nodecg.Replicant('team1style');
 const team2styleRep = nodecg.Replicant('team2style');
 
@@ -18,8 +21,10 @@ team2nameRep.on('change', (newVal) => {
 
 team1styleRep.on('change', (newVal) =>{
     team1nameEl.style.backgroundColor = newVal;
+    team1scoreboxEl.style.backgroundColor = newVal;
 })
 
 team2styleRep.on('change', (newVal) =>{
     team2nameEl.style.backgroundColor = newVal;
+    team2scoreboxEl.style.backgroundColor = newVal;
 })
