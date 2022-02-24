8dd1355e5ee92e3d8aec6d0db14870339f914a2e
themicrocheeze <themicrocheez@gmail.com>
2022-02-23 21:06:56 -0500

    font add and scoreboard customization completed

--- dashboard/teamNamesdash.js
+++ dashboard/teamNamesdash.js
@@ -4,6 +4,9 @@ const team2nameRep = nodecg.Replicant('team2name', {defaultValue: "Talon"});
 const team1styleRep = nodecg.Replicant('team1style', {defaultValue: "FFFFFF"});
 const team2styleRep = nodecg.Replicant('team2style', {defaultValue: "FFFFFF"});
 
+const team1style2Rep = nodecg.Replicant('team1textcolor', {defaultValue: "#000000"});
+const team2style2Rep = nodecg.Replicant('team2textcolor', {defaultValue: "#000000"});
+
 
 
 function setTeamNames(){
@@ -14,4 +17,6 @@ function setTeamNames(){
 function setTeamStyles(){
     team1styleRep.value = document.getElementById('team1styleinput').value;
     team2styleRep.value = document.getElementById('team2styleinput').value;
+    team1style2Rep.value = document.getElementById('team1textcolor').value;
+    team2style2Rep.value = document.getElementById('team2textcolor').value;
 }
--- graphics/teamnames.js
+++ graphics/teamnames.js
@@ -10,6 +10,10 @@ const team2scoreboxEl = document.getElementById('talonScore');
 const team1styleRep = nodecg.Replicant('team1style');
 const team2styleRep = nodecg.Replicant('team2style');
 
+const team1text = nodecg.Replicant('team1textcolor');
+const team2text = nodecg.Replicant('team2textcolor');
+
+
 
 team1nameRep.on('change', (newVal) => {
 	team1nameEl.innerHTML = newVal;
@@ -27,4 +31,18 @@ team1styleRep.on('change', (newVal) =>{
 team2styleRep.on('change', (newVal) =>{
     team2nameEl.style.backgroundColor = newVal;
     team2scoreboxEl.style.backgroundColor = newVal;
+})
+
+team1text.on('change', (newVal) =>{
+    team1nameEl.style.color = newVal;
+    team1scoreboxEl.style.color = newVal;
+    team1nameEl.style.boxShadow = "5px 5px 5px 5px " + newVal;
+    team1scoreboxEl.style.boxShadow = "5px 5px 5px 5px " + newVal;
+})
+
+team2text.on('change', (newVal) =>{
+    team2nameEl.style.color = newVal;
+    team2scoreboxEl.style.color = newVal;
+    team2nameEl.style.boxShadow = "5px 5px 5px 5px " + newVal;
+    team2scoreboxEl.style.boxShadow = "5px 5px 5px 5px " + newVal;
 })
